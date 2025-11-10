6ed8b68ed03db0bcaa820a2cd42f4ec45cade0a6
Dean Oki <75497106+Deanoki@users.noreply.github.com>
2025-11-10 21:34:33 +0700

    Update Movie.jsx

--- src/components/Movie.jsx
+++ src/components/Movie.jsx
@@ -32,13 +32,18 @@ const Movie = ({ item }) => {
       <img className="w-full h-auto block" src={`https://image.tmdb.org/t/p/w500/${item?.backdrop_path}`} alt={item?.title} />
       <div className="absolute top-0 left-0 w-full h-full hover:bg-black/80 opacity-0 hover:opacity-60 text-white">
         <p className=" whitespace-normal text-xs md:text-sm font-bold flex justify-center items-center h-full text-center">{item?.title}</p>
-        <p onClick={savedShows}>{like ? <FaHeart className="absolute top-4 left-4 text-gray-300" /> : <FaRegHeart className="absolute top-4 left-4 text-gray-300" />}</p>
+        <p onClick={savedShows}>{saved ? (
+            <FaHeart className="absolute top-4 left-4 text-red-500" />
+          ) : (
+            <FaRegHeart className="absolute top-4 left-4 text-gray-300" />
+          )}</p>
       </div>
     </div>
   );
 };
 
 export default Movie;
+
 
 
 
